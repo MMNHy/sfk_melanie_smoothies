5967bd5fe6037c6767715b3c6cff40be6603ab00
MMNHy <165206706+MMNHy@users.noreply.github.com>
2024-03-27 20:33:36 +0100

    Update streamlit_app.py
    Added title and subtitle change

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.title("My Parents New Healthy Diner")
 st.write(
     """
-    Choose the fruits you want to in Your custom Smoothie!
+    Breakfast menu
     """
 )
 #option = st.selectbox('How can we contact you?',('E-mail','Landline','Mobile'))
